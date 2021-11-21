3dcfe0d4c406d2073a08abf9ed05a4aebfb58975
AM0726Github <Margaryanalik7@gmail.com>
2021-11-20 18:13:07 -0800

    adding shoping options

--- assets/js/game.js
+++ assets/js/game.js
@@ -74,7 +74,7 @@ var startGame = function() {
   debugger;
   // reset player stats
   playerHealth = 100;
-  playerAttack = 10;
+  playerAttack = 120;
   playerMoney = 10;
 
   for (var i = 0; i < enemyNames.length; i++) {
@@ -94,6 +94,16 @@ var startGame = function() {
 
       // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
       fight(pickedEnemyName);
+
+      //if player is still alive and if we are not at the last enemy in the array
+      if (playerHealth > 0 && i< enemyNames.length - 1){
+        // ask if player wantsto use the store before next round
+        var storeConfirm = window.confirm("The fight is over, visit the store before next round?")
+        // if yes, take them to the store() funktion
+        if (storeConfirm){
+          shop();
+        }
+      }
     }
     // if player isn't alive, stop the game
     else {
@@ -127,6 +137,13 @@ var endGame = function() {
   else {
     window.alert("Thank you for playing Robot Gladiators! Come back soon!");
   }
+};
+// expression of the shop function
+var shop = function() {
+  // ask player what they'd like to do
+  var shopOptionPrompt =  window.prompt(
+    "Would you like REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+  )
 };
 
 // start the game when the page loads
